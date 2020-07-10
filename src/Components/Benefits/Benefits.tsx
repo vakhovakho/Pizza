6b13b2609e7a10eb00be6ce74e90d526a418c50d
unknown <vakho.nakashidze@gmail.com>
2020-07-10 23:09:10 +0400

    Make changes in pizza menu markup

--- src/Components/Benefits/Benefits.tsx
+++ src/Components/Benefits/Benefits.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './Benefits.module.css';
 
-const Benefits = () => {
-    return (<p className={ styles.Benefits }> Benefits </p>);
+const Benefits = (props: any) => {
+    return (
+        <div className={ styles.Benefits }>
+            <div>
+                {/* <img></img>
+                <img></img> */}
+                <p>Free delivery</p>
+                <p>lorem lorem lorem lorem lorem lorem lorem lorem</p>
+            </div>
+        </div>
+    );
 }
 
 export default Benefits;
