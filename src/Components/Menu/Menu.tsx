4e376928f857e0450a32d8d35432e76ef019d15d
unknown <vakho.nakashidze@gmail.com>
2020-07-12 23:45:19 +0400

    markup for Menu

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -5,14 +5,55 @@ import styles from './Menu.module.css';
 const Menu = () => {
     return (
     <div className={ styles.Menu }> 
-        <Pizza image='/images/photos/pizza-1.jpg' alt="pizza one"/>
-        <Pizza image='/images/photos/pizza-2.jpg' alt="pizza two"/>
-        <Pizza image='/images/photos/pizza-3.jpg' alt="pizza three"/>
-        <Pizza image='/images/photos/pizza-4.jpg' alt="pizza four"/>
-        <Pizza image='/images/photos/pizza-5.jpg' alt="pizza five"/>
-        <Pizza image='/images/photos/pizza-6.jpg' alt="pizza six"/>
-        <Pizza image='/images/photos/pizza-7.jpg' alt="pizza seven"/>
-        <Pizza image='/images/photos/pizza-8.jpg' alt="pizza eight"/>
+        <Pizza 
+            image='/images/photos/pizza-1.jpg' 
+            alt="pizza one"
+            title="name of first pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry11."
+            />
+        <Pizza 
+            image='/images/photos/pizza-2.jpg' 
+            alt="pizza two"
+            title="name of second pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry22."/>
+        <Pizza 
+            image='/images/photos/pizza-3.jpg' 
+            alt="pizza three"
+            title="name of third pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry33."/>
+        <Pizza 
+            image='/images/photos/pizza-4.jpg' 
+            alt="pizza four"
+            title="name of fourth pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry44."/>
+        <Pizza 
+            image='/images/photos/pizza-5.jpg' 
+            alt="pizza five"
+            title="name of fifth pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry55."/>
+        <Pizza 
+            image='/images/photos/pizza-6.jpg' 
+            alt="pizza six"
+            title="name of sixth pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry66."/>
+        <Pizza 
+            image='/images/photos/pizza-7.jpg' 
+            alt="pizza seven"
+            title="name of seventh pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry77."/>
+        <Pizza 
+            image='/images/photos/pizza-8.jpg' 
+            alt="pizza eight"
+            title="name of eigth pizza"
+            description="Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry88."/>
     </div>);
 }
 
--- src/Components/Menu/Pizza/Pizza.tsx
+++ src/Components/Menu/Pizza/Pizza.tsx
@@ -6,24 +6,20 @@ const Pizza = (props: any) => {
         <div className={ styles.Pizza }> 
             <img src={ props.image } alt="pizza pic" />
             <div className={ styles.ProductName }>
-                <p>სახელი</p>
+                <p>{ props.title }</p>
             </div>
             <div className={ styles.ProductDescription }>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.</p>
+                <p>{ props.description }</p>
             </div>
             <div className={ styles.ProductSizes }>
                 <div className={ styles.FirstSize}>
                     <p>25 sm</p>
                 </div>
                 <div className={ styles.FirstSize}>
-                    <p>25 sm</p>
+                    <p>35 sm</p>
                 </div>
                 <div className={ styles.FirstSize}>
-                    <p>25 sm</p>
+                    <p>45 sm</p>
                 </div>
             </div>
             <div className={ styles.ProductFooter }>
@@ -31,7 +27,7 @@ const Pizza = (props: any) => {
                     <p>100 $</p>
                 </div>
                 <div className={ styles.GetInBag }>
-                    <img src="/images/icons/shoppingbag-icon.png" alt="shopping bag icon"></img>
+                    <img src="/images/icons/cart.png" alt="shopping bag icon"></img>
                 </div>
             </div>
         </div>
