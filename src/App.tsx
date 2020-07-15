fcc79cdeef1680363526138bebfe6424be17fa56
unknown <vakho.nakashidze@gmail.com>
2020-07-16 02:29:24 +0400

    My orders markup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Cart from './Containers/Cart/Cart';
 import Wrapper from './Components/UI/Wrapper/Wrapper';
 import { Switch, Route, BrowserRouter} from "react-router-dom";
 import './App.css';
+import MyOrder from './Components/MyOrders/MyOrders';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Wrapper>
       <Switch>
         <Route path="/" exact component={ Home } />
+        <Route path="/my-orders" exact component={ MyOrder } />
         <Route path="/cart" component={ Cart } />
       </Switch>
     </Wrapper>
