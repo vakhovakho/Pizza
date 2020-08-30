b3580e32d78461938b3ac8b9a43d105656856482
Vakho Nakashidze <vakho@Vakhos-MacBook-Pro.local>
2020-08-30 16:22:49 +0400

    favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Home from './Containers/Home/Home';
 import Cart from './Containers/Cart/Cart';
 import Wrapper from './Components/UI/Wrapper/Wrapper';
